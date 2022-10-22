54bab8ffb2c4e2a596cad351bedde9522fc60bad
CRachel23 <jan.connie23@gmail.com>
2022-10-22 12:24:17 +0800

    1st try

---
+++ test1.py
@@ -0,0 +1,5 @@
+import telepot
+
+bot = telepot.Bot("5617446728:AAG7DQL3IQG1H1dMoHvYWqEM7K9sPmCK7yw")
+
+bot.sendPhoto(chat_id="449959530", photo=("https://i.guim.co.uk/img/media/fe1e34da640c5c56ed16f76ce6f994fa9343d09d/0_174_3408_2046/master/3408.jpg?width=620&quality=85&dpr=1&s=none"))
